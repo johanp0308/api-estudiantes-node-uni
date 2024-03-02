0959f575bb2d9051c611101640e187f5997d468a
johano0308 <sebastian-johan1@hotmail.com>
2024-03-02 16:56:40 -0500

    feat: :sparkles: Finished Crud estudiante

--- src/controller/estudiantes.controller.js
+++ src/controller/estudiantes.controller.js
@@ -1,6 +1,43 @@
 import {pool} from '../db.js';
 
 
+export const obtnerTodosEstudiantes = async (req,res)=>{
+    try{
+        let statemet = "SELECT * FROM estudiantes;";
+        let resultado = await pool.query(statemet,[]);
+        let datos = resultado[0];
+
+        if(datos.length > 0){
+            console.log(datos);
+            return res.status(200).json(datos);
+        }else{
+            return res.status(204).json();
+        }
+    }catch (e){
+        console.log(`Ha ocurriod un error: ${e}`);
+        return res.status(500).json({message:`Ocurrio un error de MySQL, mensaje: ${e.sqlMessage}`});
+    }
+}
+
+export const obtnerEstudianteById = async (req,res)=>{
+    try{
+        let idEstudiante = req.params.id
+        let statemet = "SELECT * FROM estudiantes WHERE estudiante_id = ?;";
+        let resultado = await pool.query(statemet,[idEstudiante]);
+        let datos = resultado[0];
+
+        if(datos.length > 0){
+            console.log(datos);
+            return res.status(200).json(datos);
+        }else{
+            return res.status(204).json();
+        }
+    }catch (e){
+        console.log(`Ha ocurriod un error: ${e}`);
+        return res.status(500).json({message:`Ocurrio un error de MySQL, mensaje: ${e.sqlMessage}`});
+    }
+}
+
 export const createEstudiante = async (req,res)=> {
     try{
         let {nombre_estudiante,apellido_estudiante,fecha_nacimiento} = req.body;
@@ -19,3 +56,37 @@ export const createEstudiante = async (req,res)=> {
         return res.status(500).json({message:`Ocurrio un error de MySQL, mensaje: ${e.sqlMessage}`});
     }
 };
+
+export const deleteEstudiante = async (req,res)=>{
+    try{
+        let idEstudiante = req.params.id;
+        let statemet = "DELETE FROM estudiantes WHERE estudiante_id = ?;";
+        let resultado = await pool.query(statemet,[idEstudiante]);
+        if(resultado[0].affectedRows == 1){
+            return res.status(200).json({message:"Estudiante eliminado."})
+        }else{
+            res.status(204).json();
+        }
+    }catch (e){
+        console.log(`Ha ocurriod un error: ${e}`);
+        return res.status(500).json({message:`Ocurrio un error de MySQL, mensaje: ${e.sqlMessage}`});
+    }
+};
+
+export const actualizarEstudiante = async (req,res) =>{
+    try{
+        let idEstudiante = req.params.id;
+        let {nombre_estudiante,apellido_estudiante,fecha_nacimiento} = req.body;
+        let datos = [nombre_estudiante,apellido_estudiante,fecha_nacimiento,idEstudiante];
+        let statemet = "UPDATE estudiantes SET nombre_estudiante = ?, apellido_estudiante = ?, fecha_nacimiento = ? WHERE estudiante_id = ?;";
+        let resultado = await pool.query(statemet,datos);
+        if(resultado[0].affectedRows == 1){
+            return res.status(200).json({message:"Estudiante acutializado."})
+        }else{
+            res.status(204).json();
+        }
+    }catch (e){
+        console.log(`Ha ocurriod un error: ${e}`);
+        return res.status(500).json({message:`Ocurrio un error de MySQL, mensaje: ${e.sqlMessage}`});
+    }
+}
--- src/routes/estudiante.routes.js
+++ src/routes/estudiante.routes.js
@@ -1,9 +1,12 @@
 import { Router } from 'express';
-import {createEstudiante} from '../controller/estudiantes.controller.js';
+import {createEstudiante, obtnerTodosEstudiantes, deleteEstudiante,obtnerEstudianteById,actualizarEstudiante} from '../controller/estudiantes.controller.js';
 
 const router = Router();
 
 router.post('/',createEstudiante);
-
+router.get('/',obtnerTodosEstudiantes);
+router.get('/id/:id',obtnerEstudianteById)
+router.delete('/id/:id',deleteEstudiante);
+router.put('/id/:id',actualizarEstudiante);
 
 export default router;
